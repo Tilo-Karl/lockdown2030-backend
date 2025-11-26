9629fe7141b71ccd2b86eb1c7cec7785a85e3ffb
tilo_delau <tdelau@gmail.com>
2025-11-26 14:44:27 +0100

    firestore didnt like how i stored rows

--- ld2030/v1/map-gen.js
+++ ld2030/v1/map-gen.js
@@ -121,7 +121,7 @@ function generateMap({
       params: { buildingChance, minLabDistance },
       buildings,
       buildingPalette: MAP.BUILDING_PALETTE,
-      terrain: rows,
+      terrain: rows.map(r => r.join('')),
     },
   };
 }
